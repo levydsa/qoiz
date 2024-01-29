dec6eb0fa6969e838b44668056419fcc03cf54e6
Levy Albuquerque <levyddsa@gmail.com>
2024-01-29 02:32:17 -0300

    fix(build): update build.zig to new api

--- build.zig
+++ build.zig
@@ -6,47 +6,45 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("qoiz", .{
-        .source_file = LazyPath.relative("src/qoiz.zig"),
+        .root_source_file = .{ .path = "src/qoiz.zig" },
     });
 
     const bench = b.addExecutable(.{
         .name = "bench",
-        .root_source_file = LazyPath.relative("src/bench.zig"),
+        .root_source_file = .{ .path = "src/bench.zig" },
         .target = target,
         .optimize = optimize,
     });
 
-    b.installArtifact(bench);
     bench.addCSourceFile(.{
-        .file = LazyPath.relative("src/bench/qoi_impl.c"),
+        .file = .{ .path = "src/bench/qoi_impl.c" },
         .flags = &.{"-O3"},
     });
-    bench.addIncludePath(LazyPath.relative("src/bench/"));
+    bench.addIncludePath(.{ .path = "src/bench/" });
     bench.linkLibC();
 
     {
         const run = b.addRunArtifact(bench);
-        if (b.args) |args| run.addArgs(args);
+        b.step("bench", "Run benchmarks").dependOn(&run.step);
 
-        const step = b.step("bench", "Run benchmarks");
-        step.dependOn(&run.step);
+        if (b.args) |args| run.addArgs(args);
     }
 
     const unit_tests = b.addTest(.{
-        .root_source_file = LazyPath.relative("src/qoiz.zig"),
+        .root_source_file = .{ .path = "src/qoiz.zig" },
         .target = target,
         .optimize = optimize,
     });
+
     unit_tests.addCSourceFile(.{
-        .file = LazyPath.relative("src/bench/qoi_impl.c"),
+        .file = .{ .path = "src/bench/qoi_impl.c" },
         .flags = &.{"-O3"},
     });
-    unit_tests.addIncludePath(LazyPath.relative("src/bench/"));
+    unit_tests.addIncludePath(.{ .path = "src/bench/" });
     unit_tests.linkLibC();
 
     {
         const run = b.addRunArtifact(unit_tests);
-        const step = b.step("test", "Run unit tests");
-        step.dependOn(&run.step);
+        b.step("test", "Run unit tests").dependOn(&run.step);
     }
 }
